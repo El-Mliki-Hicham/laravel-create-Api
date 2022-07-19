cf2ee367bbb498af6fa7ec41466268e4984edebb
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-07-19 15:04:08 +0200

    api test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\ProduitModel;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -10,4 +11,25 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+
+    // function apiTest($id=null){
+    //     $data = $id?ProduitModel::find($id):ProduitModel::all();
+    //     return $data;
+
+    // }
+    function apiTest(){
+        $data = ProduitModel::all();
+        return $data;
+
+    }
+    function apiTestId($id){
+        $data = ProduitModel::where("id_produit","=",$id)->get();
+        return $data;
+
+    }
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+route::get("data/",[Controller::class,'apiTest']);
+route::get("data/{id}",[Controller::class,'apiTestId']);
